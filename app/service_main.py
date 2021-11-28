60fa6886c794e61fa6934bb7643497d07396ef1a
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-11-28 11:56:24 -0500

    Se agregaron las rutas esperadas para los servicios

--- app/service_main.py
+++ app/service_main.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def informacion_nodo():
+    pass
+@app.route('suma_de_red', methods=['GET'])
+def sumar_red():
+    pass
+@app.route('guardar_numero', methods=['POST'])
+def anadir_numero():
+    pass
+
 
 
 def start(ip:str, port:int):

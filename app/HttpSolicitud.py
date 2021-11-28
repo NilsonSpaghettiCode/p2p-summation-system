f82e35f8fc4c0d892ecee875548e231dca44a6f6
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-11-28 16:27:01 -0500

    Se agrega liberia al requeriments con nombre request

--- app/HttpSolicitud.py
+++ app/HttpSolicitud.py
@@ -1,2 +1,13 @@
+'''
+Clase HttpSolicitud
+-------------------
+Esta clase contiene los metodos principales para consumir
+los servicios de los demas nodos
+'''
+
+import requests
 class HttpSolicitud:
-    pass
+    '''
+    '''
+    def __init__(self):
+        pass
--- config/config.py
+++ config/config.py
@@ -4,12 +4,12 @@ Archivo config
 Este modulo contiene la configuración del nodo
 '''
 config = {
+    'nombre_nodo':'Nodo A',
     'ip_address': '0.0.0.0',
     'port':5200,
     'nodos_conocidos':[
         {'nombre_nodo':'Nodo B', 'ip':'0.0.0.0', 'puerto':5200},
         {'nombre_nodo':'Nodo C', 'ip':'0.0.0.0', 'puerto':5200},
         {'nombre_nodo':'Nodo D', 'ip':'0.0.0.0', 'puerto':5200}
-    ],
-    'nombre_nodo':'Nodo A',
+    ]
 }
